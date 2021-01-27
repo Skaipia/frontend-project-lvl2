a6dc44f5225596bcccf8f562a112380e8efb2daa
jestjestjest <kethup2@gmail.com>
2021-01-28 00:50:21 +0300

    start

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 import program from 'commander'
 
-program.version('0.0.1');
+program.version('0.0.1')
+    .option('-V, --version', 'output the version number')
+    .option('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
